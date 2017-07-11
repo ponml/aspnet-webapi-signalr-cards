2a1bc4a70d6d432d4c83ea438e644afa75157a34
chris <ccsand@hotmail.com>
2017-07-10 20:06:15 -0700

    ChatWindow

---
+++ aspnet-webapi-signalr-cards/Scripts/ChatWindow.js
@@ -0,0 +1,97 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ChatWindow extends React.Component {
+    constructor(props) {
+        super(props);
+        var me = this;
+        me.chatHub = props.chatHub;
+        me.state = {
+            messages: [],
+            msgBoxValue: "",
+            cardId: "",
+            foundCard: null
+        };
+
+        me.handleCardSubmit = me.handleCardSubmit.bind(me);
+        me.handleCardOnChange = me.handleCardOnChange.bind(me);
+        me.handleMsgBoxSubmit = me.handleMsgBoxSubmit.bind(me);
+        me.handleMsgBoxOnChange = me.handleMsgBoxOnChange.bind(me);
+
+        me.chatHub.client.broadcastMessage = function (name, message) {
+            // Html encode display name and message.
+            var currentMessages = me.state.messages;
+            var newMessage = {
+                name: name,
+                msg: message
+            };
+            currentMessages.push(newMessage);
+            me.setState({ messages: currentMessages });
+        };
+    }
+
+    handleCardSubmit(e) {
+        var me = this;
+        if (e.keyCode == 13) {
+            return axios.get("api/cards", { params: { id: me.state.cardId } }).then(function (response) {
+                me.setState({
+                    foundCard: response.data
+                });
+            });
+        }
+    }
+
+    handleCardOnChange(e) {
+        var me = this;
+        me.setState({
+            cardId: e.target.value
+        });
+    }
+
+    handleMsgBoxSubmit(e) {
+        var me = this;
+        if (e.keyCode == 13) {
+            me.chatHub.server.send(me.chatHub.connection.id, me.state.msgBoxValue);
+            me.setState({
+                msgBoxValue: ""
+            });
+        }
+    }
+
+    handleMsgBoxOnChange(e) {
+        var me = this;
+        me.setState({
+            msgBoxValue: e.target.value
+        });
+    }
+
+
+    render() {
+
+        var foundCard = this.state.foundCard ? <Card data={this.state.foundCard} /> : null;
+        return (
+            <div>
+                <input type="text" placeholder={"type here"} value={this.state.msgBoxValue} onKeyUp={this.handleMsgBoxSubmit} onChange={this.handleMsgBoxOnChange} />
+                <ul>
+                    {
+                        this.state.messages.map((msg, index) => {
+                            return (
+                                <li key={index}>
+                                    <span>{msg.name}</span>
+                                    <span>{"::"}</span>
+                                    <span>{msg.msg}</span>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+                <input type="text" placeholder={"get card by id"} value={this.state.cardId} onKeyUp={this.handleCardSubmit} onChange={this.handleCardOnChange} />
+                <div>
+                    {foundCard}
+                </div>
+            </div>
+        );
+    }
+}
+
+module.exports = ChatWindow;
--- aspnet-webapi-signalr-cards/index.js
+++ aspnet-webapi-signalr-cards/index.js
@@ -1,7 +1,8 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import Card from './Scripts/Card.js'
+import Card from './Scripts/Card.js';
+import ChatWindow from './Scripts/ChatWindow.js';
 
 var uri = 'api/cards';
 
@@ -28,7 +29,6 @@ $(document).ready(function () {
 class App extends React.Component {
     init(props) {
         var me = this;
-        debugger;
         // Send an AJAX request
         return axios.get("api/cards").then(function (response) {
             // On success, 'data' contains a list of cards.
@@ -47,7 +47,9 @@ class App extends React.Component {
         me.state = {
             cards: []
         };
-        me.chat = $.connection.chatHub
+        me.signalRConnection = $.connection;
+        me.signalRConnection.hub.start().done(function () { });
+        me.chatHub = me.signalRConnection.chatHub;
         me.init(props);
     }
 
@@ -70,109 +72,18 @@ class App extends React.Component {
     render() {
         return (
             <div>
+                <div className="container">
+                    <ChatWindow chatHub={this.chatHub} />
+                </div>
                 <div>
                     <h2>All Cards</h2>
                     <ul id="cards">
                         { this.state.cards }
                     </ul>
                 </div>
-                <div className="container">
-                    <ChatWindow />
-                </div>
             </div>
         );
     }
 
 }
 
-class ChatWindow extends React.Component {
-    constructor(props) {
-        super(props);
-        var me = this;
-        me.chat = $.connection.chatHub;
-        me.state = {
-            messages: [],
-            msgBox: "",
-            cardId: "",
-            foundCard: null
-        };
-
-        this.handleCardSubmit = this.handleCardSubmit.bind(this);
-        this.handleCardOnChange = this.handleCardOnChange.bind(this);
-        this.handleMsgBoxSubmit = this.handleMsgBoxSubmit.bind(this);
-        this.handleMsgBoxOnChange = this.handleMsgBoxOnChange.bind(this);
-
-        me.chat.client.broadcastMessage = function (name, message) {
-            // Html encode display name and message.
-            var message = {
-                name: name,
-                msg: message
-            };
-            var messages = this.state.messages.push(message);
-            this.setState({ messages: messages });
-        };
-
-        $.connection.hub.start().done(function () {
-
-        });
-    }
-
-    handleCardSubmit(e) {
-        var me = this;
-        if (e.keyCode == 13) {
-            return axios.get("api/cards", { params: { id: this.state.cardId } }).then(function (response) {
-                me.setState({
-                    foundCard: response.data
-                });
-            });
-        }
-    }
-
-    handleCardOnChange(e) {
-        var me = this;
-        me.setState({
-            cardId: e.target.value
-        });
-    }
-
-    handleMsgBoxSubmit(e) {
-        var me = this;
-        if (e.keyCode == 13) {
-            return axios.get(uri, { params: { id: me.state.cardId } }).then(function (response) {
-
-            });
-        }
-    }
-
-    handleMsgBoxOnChange(e) {
-        var me = this;
-        me.setState({
-            msgBox: e.target.value
-        });
-    }
-
-
-    render() {
-
-        var foundCard = this.state.foundCard ? <Card data={this.state.foundCard} /> : null;
-        return (
-            <div>
-                <input type="text" placeholder={"type here"} value={this.state.msgBox} onKeyUp={this.handleMsgBoxSubmit} onChange={this.handleMsgBoxOnChange} />
-                <ul>
-                    {
-                        this.state.messages.map(msg => {
-                            return
-                            <li>
-                                <span>msg.name</span>::<span>msg.msg</span>
-                            </li>
-                        })
-                    }
-                </ul>
-                <input type="text" placeholder={"get card by id"} value={this.state.cardId} onKeyUp={this.handleCardSubmit} onChange={this.handleCardOnChange} />
-                <div>
-                    {foundCard}
-                </div>
-            </div>
-        );
-    }
-}
